f71555072376b70210b80833c83d72047c17a29c
Alwan-ar <muhammadalwanarfianto2013@gmail.com>
2020-06-19 09:07:46 +0700

    movie

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div >
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+    <search-component/>
+    <movie-component/>
+  </div>
+</template>
+
+<script>
+import Search from './components/SearchMovie';
+import Movie from './components/MovieComponent';
+export default {
+  name: 'App',
+  components: {
+    'search-component' : Search,
+    'movie-component': Movie,
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
